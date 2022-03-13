85c6b528d3d0d4a4a7900e4496b72486876d846c
ugurcanatas <ugurcanemre93@gmail.com>
2022-03-13 13:34:14 +0300

    🛠  ios-linking [MODIFIED] ios functions fixed. Added / to exec function. added filter function for CUSTOM_ACTIONS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-const inquirer = require("inquirer");
-const { androidEmulatorList, androidDeviceAction } = require("./android");
-const { iosRuntimeList, iosEmulatorList, iosDeviceAction } = require("./ios");
+const inquirer = require('inquirer');
+const { androidEmulatorList, androidDeviceAction } = require('./android');
+const { iosRuntimeList, iosEmulatorList, iosDeviceAction } = require('./ios');
 const {
   OS_TYPE_Q,
   IOS_ACTION_CHOICES,
   ANDROID_ACTION_CHOICES,
-} = require("./constants");
+  IOS_ACTION_IDENTIFIERS
+} = require('./constants');
 
 const pickOsType = () => {
   return OS_TYPE_Q;
@@ -16,37 +17,56 @@ const pickOsType = () => {
 const pickAction = (type, selected) => {
   const prompts = {
     ios: {
-      type: "list",
-      name: "action",
+      type: 'list',
+      name: 'action',
       message: `Select a process for ${selected}`,
-      choices: IOS_ACTION_CHOICES,
+      choices: IOS_ACTION_CHOICES
     },
     android: {
-      type: "list",
-      name: "action",
+      type: 'list',
+      name: 'action',
       message: `Select a process for ${selected}`,
-      choices: ANDROID_ACTION_CHOICES,
-    },
+      choices: ANDROID_ACTION_CHOICES
+    }
   };
   return prompts[type];
 };
 
+const iosActionFilter = async identifier => {
+  switch (identifier) {
+    case IOS_ACTION_IDENTIFIERS.CUSTOM_ACTION:
+      const { url } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'url',
+          message: 'URL Input'
+        }
+      ]);
+      return url;
+    default:
+      break;
+  }
+};
+
 async function main() {
   const { os_type } = await inquirer.prompt([pickOsType()]);
-  if (os_type === "IOS") {
+  if (os_type === 'IOS') {
     const { ios_runtime } = await inquirer.prompt([await iosRuntimeList()]);
     const { ios_device } = await inquirer.prompt([
-      await iosEmulatorList(ios_runtime),
+      await iosEmulatorList(ios_runtime)
     ]);
     const { deviceName, deviceUDID, deviceState } = JSON.parse(ios_device);
-    const { action } = await inquirer.prompt([pickAction("ios", deviceName)]);
-    iosDeviceAction(action, deviceUDID, deviceState);
+    const {
+      action: { type, identifier }
+    } = await inquirer.prompt([pickAction('ios', deviceName)]);
+    const filteredActionResults = await iosActionFilter(identifier);
+    iosDeviceAction(type, deviceUDID, deviceState, filteredActionResults);
   } else {
     const { selected_emualator } = await inquirer.prompt([
-      await androidEmulatorList(),
+      await androidEmulatorList()
     ]);
     const { action } = await inquirer.prompt([
-      pickAction("android", selected_emualator),
+      pickAction('android', selected_emualator)
     ]);
     androidDeviceAction(action, selected_emualator);
   }
