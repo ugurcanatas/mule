73efd603658db090dcb180916a1211942a06b07c
ugurcanatas <ugurcanemre93@gmail.com>
2022-03-15 00:32:43 +0300

    ✨  help-command [ADDED] help.js added for displaying help commands. TODOS present for now.

---
+++ src/help.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * TODO: Add help commands here
+ * use 
+ * process.argv.forEach((val, index) => {
+ * console.log(`${index}: ${val}`)
+ * })
+ * to get --help or -h flag
+ */
