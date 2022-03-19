dffda48495ff1ede1f791a89701a3b1a8370b68f
Ugurcan Emre Atas <22047299+ugurcanatas@users.noreply.github.com>
2022-03-19 19:41:28 +0300

    🛠  custom-spinners [MODIFIED] testing color codes for displaying colorful messages

--- src/utils/spinners.js
+++ src/utils/spinners.js
@@ -1,15 +1,40 @@
 const readline = require('readline');
 
 const spinnerOptions = {
-  lines: {
-    type: 'lines',
+  halfCircles: {
+    type: 'halfCircles',
     intervalTime: 80,
     symbols: ['◐', '◓', '◑', '◒']
+  },
+  dotsLine: {
+    type: 'dotsLine',
+    intervalTime: 100,
+    symbols: ['.', '..', '...', '....']
   }
 };
 
+const fontTypes = {
+  bold: [1, 22],
+  dim: [2, 22],
+  italic: [3, 23]
+};
+
+const colors = {
+  YELLOW: [33, 89],
+  PURPLE: [22, 0],
+  RED: [31, 89]
+};
+const generateColor = (message, colorName) => {
+  const [first, second] = colors[colorName];
+  return `\x1b[${first}m${message}\x1b[${second}m`;
+};
+
+// const generateTextStyle = (message, style) => {
+//   fontTypes[style];
+// };
+
 class Spinner {
-  constructor({ message, type = 'lines' }) {
+  constructor({ message, type = 'dotsLine' }) {
     this.message = message;
     this.type = type;
   }
@@ -24,20 +49,21 @@ class Spinner {
 
   startSpinner() {
     const { intervalTime, symbols } = this.getSpinnerDataByType();
-    !!this.message && process.stdout.write(`\x1b[34m${this.message}\x1b[0m`);
+    !!this.message && process.stdout.write(generateColor(this.message, 'RED')); // `\x1b[34m${this.message}\x1b[0m`;
     let counter = 0;
     this.spinnerInterval = setInterval(() => {
       if (!symbols[counter]) {
         counter = 0;
       }
       readline.cursorTo(process.stdout, this.message.length + 1 || 0);
-      process.stdout.write(`${symbols[counter]}`);
+      process.stdout.write(generateColor(`${symbols[counter]}`, 'RED'));
       counter = counter === symbols.length ? 0 : counter + 1;
     }, intervalTime);
   }
 
   stopSpinner() {
-    process.stdout.write('\n');
+    this.message = ' \n';
+    process.stdout.write(this.message);
     this.spinnerInterval && clearInterval(this.spinnerInterval);
     this.spinnerInterval = null;
     this.message = '';
