f7f05725f906b5842ec7b0db35f27ac0e50affb0
Ugurcan Emre Atas <22047299+ugurcanatas@users.noreply.github.com>
2022-03-20 14:28:43 +0300

    ✨  colors-n-text [ADDED] colors moved to its own js file

---
+++ src/utils/colors/index.js
@@ -0,0 +1,27 @@
+const RGB_COLORS = {
+  RED_500: {
+    red: 244,
+    green: 67,
+    blue: 54
+  },
+  PINK_500: {
+    red: 233,
+    green: 30,
+    blue: 89
+  },
+  INDIGO_500: {
+    red: 67,
+    green: 81,
+    blue: 181
+  },
+  AMBER_500: {
+    red: 255,
+    green: 193,
+    blue: 7
+  }
+};
+
+const RGB_BG_PREFIXES = ['\x1b[48;2;', '\x1b[0m'];
+const RGB_FG_PREFIXES = ['\x1b[38;2;', '\x1b[0m'];
+
+module.exports = { RGB_COLORS, RGB_BG_PREFIXES, RGB_FG_PREFIXES };
